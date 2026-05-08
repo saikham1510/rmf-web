7148bd4a2b5a430d76e30cdaa60e2565c6aae7f4
saikham1510 <saikham6276@gmail.com>
2026-05-08 09:33:23 +0800

    feat(appbar): enable 'Show Schedule' button in CreateTaskForm

--- packages/dashboard/src/components/appbar.tsx
+++ packages/dashboard/src/components/appbar.tsx
@@ -911,7 +911,7 @@ export const AppBar = React.memo(({ extraToolbarItems }: AppBarProps): React.Rea
       {openCreateTaskForm && (
         <CreateTaskForm
           user={username ? username : 'unknown user'}
-          showScheduleButton={false}
+          showScheduleButton={true}
           patrolWaypoints={waypointNames}
           cleaningZones={cleaningZoneNames}
           pickupPoints={pickupPoints}
